8f76d67cd29df542bd61b4b8c41e3f75c7fcd04a
Shree <Shree@DESKTOP-GCPK288>
2020-05-16 07:17:33 +0530

    First Commit

---
+++ Maven_GroupID_jan19/src/main/java/Maven_GroupID_jan19/Maven_GroupID_jan19/App.java
@@ -0,0 +1,13 @@
+package Maven_GroupID_jan19.Maven_GroupID_jan19;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
